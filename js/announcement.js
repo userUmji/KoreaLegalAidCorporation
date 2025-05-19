2b44ae92fbddc28921d9bc6f0bfe805fe408e7e0
UmJi <147022364+userUmji@users.noreply.github.com>
2025-05-19 22:34:42 +0900

    250519

--- js/announcement.js
+++ js/announcement.js
@@ -42,7 +42,6 @@ function list_view(page_num){
   let start_num, end_num;
   start_num = page_num * 10;
   end_num = start_num + 10;
-  console.log("리스트: ",start_num, end_num);
 
   for(let i  = 0; i < totalList.length; i++){
   totalList[i].style.display = "none";
@@ -63,7 +62,6 @@ let max_btn_page = Math.ceil(maxListCount / maxViewBtnCount);
 view_pagnation(0);
 // 페이지 버튼 관리
 function view_pagnation(num){
-  console.log(num);
   let start_num, end_num;
   start_num = num * maxViewBtnCount;
   end_num = start_num + maxViewBtnCount;
@@ -117,8 +115,6 @@ document.querySelector(".next_Btn").addEventListener("click",()=>{
     list_view(cur_page_num * 10);
     active_btn(cur_page_num * 10);
   }
-  console.log(cur_page_num);
-  
 })
 // 맨 처음 번호로 이동
 document.querySelector(".start_Btn").addEventListener("click",()=>{
@@ -129,10 +125,10 @@ document.querySelector(".start_Btn").addEventListener("click",()=>{
 })
 // 맨 끝 번호로 이동
 document.querySelector(".end_Btn").addEventListener("click",()=>{
-  cur_page_num = maxListCount - 1;
+  cur_page_num = max_btn_page-1;
   view_pagnation(max_btn_page - 1);
   list_view(maxListCount - 1);
-  active_btn(cur_page_num);
+  active_btn(maxListCount -1);
 })
 
 active_btn(0);
@@ -142,99 +138,3 @@ all_btn_list.forEach((btn, index)=>{
     list_view(index);
   })
 })
-
-/*
-// 번호 생성 
-function Btn_Create(type_B, pageNum){
-  // 다음 버튼 눌렀을 때
-  if(type_B == "next"){
-    
-  }
-  // 이전 버튼
-  else if(type_B == "prev"){
-
-  }
-  // 처음 초기화
-  else{
-    for(let i = 1; i <= maxListCount; i++){
-      pagnation_num_list.innerHTML += `<li><button>${i}</button></li>`;
-    }
-    for(pL of pagnation_num_list.children){
-      pL.style.display = "none";
-    }
-    for(let i = 0; i < maxViewBtnCount; i++){
-      pagnation_num_list.children[i].style.display = "block";
-    }
-  }
-}
-
-Btn_Create(0);
-// 버튼 리스트
-let viewBtnList = pagnation_num_list.querySelectorAll("button");
-
-// 버튼 값을 입력받아 그 번호에 맞는 공지시항 on
-function View_List(num){
-  // 번호 off
-  for(vBL of viewBtnList){
-    vBL.classList.remove("active");
-  }
-  // 맞는 번호 on
-  viewBtnList[num].classList.add("active");
-  // 모든 공지사항 off
-  for(tL of totalList){
-    tL.style.display = "none";
-  }
-  // 시작 번호
-  let start = num * maxViewListCount;
-  // 끝 번호
-  let end = start + maxViewListCount;
-
-  let totalList_C = [...totalList];
-  let selectList = totalList_C.slice(start, end);
-
-  // 선택한 번호에 맞는 공지사항 on
-  for(sL of selectList){
-    sL.style.display = "";
-  }
-}
-
-View_List(nowPageNum);
-
-// 버튼 제어
-// 번호 클릭
-viewBtnList.forEach((btn, index)=>{
-  btn.addEventListener("click",()=>{
-    nowPageNum = index;
-    View_List(index);
-  })
-})
-// 맨 처음 번호로 이동
-document.querySelector(".start_Btn").addEventListener("click",()=>{
-  nowPageNum = 0;
-  View_List(nowPageNum);
-})
-// 이전 번호
-document.querySelector(".prev_Btn").addEventListener("click",()=>{
-  nowPageNum--;
-  if(nowPageNum < 0){
-    nowPageNum = 0;
-  }
-  View_List(nowPageNum);
-})
-// 다음 번호
-document.querySelector(".next_Btn").addEventListener("click",()=>{ 
-  nowPageNum++
-  if(nowPageNum > maxListCount){
-    nowPageNum = maxListCount;
-  }
-  Btn_Create("next");
-  View_List(nowPageNum);
-  console.log(start);
-  console.log(end);
-})
-// 맨 끝 번호로 이동
-document.querySelector(".end_Btn").addEventListener("click",()=>{
-  nowPageNum = maxListCount;
-  View_List(nowPageNum);
-})
-*/
